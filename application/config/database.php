b8cb67fca94f53e0fbbf2da0891eadeb09b36511
nuwe1 <marvinelly18@gmail.com>
2020-07-11 17:04:34 +0300

    Edit database config

--- application/config/database.php
+++ application/config/database.php
@@ -72,13 +72,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'honeypride',
+	'hostname' => $url["host"],
+	'username' => $url["user"],
+	'password' => $url["user"],
+	'database' => substr($url["path"], 1),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
